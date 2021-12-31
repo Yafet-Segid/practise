d384ba059f4d9e87af5e2ba5efef5a54466a231b
Yafet-Segid <yafetsegid@gmail.com>
2021-12-30 23:28:07 -0600

    github copilot

--- style.js
+++ style.js
@@ -1,3 +1,7 @@
 // setup express server
 var express = require('express');  
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var port = process.env.PORT || 3000;
+
